4a0f88b5a83c549ebc9157fd6fdc31b74c46ccc2
helenefav <helene.favrault@datadoghq.com>
2021-05-26 15:05:15 +0200

    remove dynamo

--- create.js
+++ create.js
@@ -1,6 +1,6 @@
 import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
-// import dynamoDb from "./libs/dynamodb-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
@@ -16,7 +16,7 @@ export const main = handler(async (event, context) => {
     },
   };
 
-  // await dynamoDb.put(params);
+  await dynamoDb.put(params);
 
   return params.Item;
 });
